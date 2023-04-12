8f97755c3832d4628e93e41363a8480d74dea8eb
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-12 10:37:01 -0300

    Remove unused code on site

--- apps/site/src/app/app.tsx
+++ apps/site/src/app/app.tsx
@@ -1,13 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from "./app.module.scss";
-
-import NxWelcome from "./nx-welcome";
+import styles from './app.module.scss';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="site" />
-
+      <h1>Post GPT</h1>
       <div />
     </>
   );
