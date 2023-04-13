0faff24657e856bc2c921a385bee152bdb40455a
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-13 18:04:14 -0300

    Add lib firebase

---
+++ libs/firebase/src/lib/firebase.ts
@@ -0,0 +1,63 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFunctions } from 'firebase/functions';
+import { getFirestore } from 'firebase/firestore';
+
+// Gets environment variables from process (Node) or import.meta (Browser)
+const env: {
+  FIREBASE_API_KEY?: string;
+  FIREBASE_AUTH_DOMAIN?: string;
+  FIREBASE_PROJECT_ID?: string;
+  FIREBASE_STORAGE_BUCKET?: string;
+  FIREBASE_MESSAGING_SENDER_ID?: string;
+  FIREBASE_APP_ID?: string;
+  FIREBASE_MEASUREMENT_ID?: string;
+} = {};
+env.FIREBASE_API_KEY =
+  process?.env?.FIREBASE_API_KEY || import.meta.env.VITE_FIREBASE_API_KEY;
+env.FIREBASE_AUTH_DOMAIN =
+  process?.env?.FIREBASE_AUTH_DOMAIN ||
+  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+env.FIREBASE_PROJECT_ID =
+  process?.env?.FIREBASE_PROJECT_ID || import.meta.env.VITE_FIREBASE_PROJECT_ID;
+env.FIREBASE_STORAGE_BUCKET =
+  process?.env?.FIREBASE_STORAGE_BUCKET ||
+  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+env.FIREBASE_MESSAGING_SENDER_ID =
+  process?.env?.FIREBASE_MESSAGING_SENDER_ID ||
+  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+env.FIREBASE_APP_ID =
+  process?.env?.FIREBASE_APP_ID || import.meta.env.VITE_FIREBASE_APP_ID;
+env.FIREBASE_MEASUREMENT_ID =
+  process?.env?.FIREBASE_MEASUREMENT_ID ||
+  import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+auth.useDeviceLanguage();
+const functions = getFunctions(app);
+const db = getFirestore(app);
+
+export function firebase() {
+  return {
+    app,
+    analytics,
+    auth,
+    functions,
+    db,
+  };
+}
