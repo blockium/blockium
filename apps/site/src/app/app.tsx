7420b75e98310edad36de4716a3a80963f025a5b
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-13 21:36:55 -0300

    tested .env

--- apps/site/src/app/app.tsx
+++ apps/site/src/app/app.tsx
@@ -1,12 +1,8 @@
 import { Hero } from '@postgpt/commonui';
-// import { firebase } from '@postgpt/firebase';
+// import { app } from '@postgpt/firebase';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-
-console.log(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
-// console.log(firebase);
 
 export function App() {
   return (
--- libs/firebase/src/lib/firebase.ts
+++ libs/firebase/src/lib/firebase.ts
@@ -15,24 +15,27 @@ const env: {
   FIREBASE_APP_ID?: string;
   FIREBASE_MEASUREMENT_ID?: string;
 } = {};
-env.FIREBASE_API_KEY =
-  process?.env?.FIREBASE_API_KEY || import.meta.env.VITE_FIREBASE_API_KEY;
-env.FIREBASE_AUTH_DOMAIN =
-  process?.env?.FIREBASE_AUTH_DOMAIN ||
-  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
-env.FIREBASE_PROJECT_ID =
-  process?.env?.FIREBASE_PROJECT_ID || import.meta.env.VITE_FIREBASE_PROJECT_ID;
-env.FIREBASE_STORAGE_BUCKET =
-  process?.env?.FIREBASE_STORAGE_BUCKET ||
-  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
-env.FIREBASE_MESSAGING_SENDER_ID =
-  process?.env?.FIREBASE_MESSAGING_SENDER_ID ||
-  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
-env.FIREBASE_APP_ID =
-  process?.env?.FIREBASE_APP_ID || import.meta.env.VITE_FIREBASE_APP_ID;
-env.FIREBASE_MEASUREMENT_ID =
-  process?.env?.FIREBASE_MEASUREMENT_ID ||
-  import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+
+if (typeof process !== 'undefined') {
+  env.FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+  env.FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+  env.FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+  env.FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+  env.FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+  env.FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
+  env.FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
+} else {
+  env.FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+  env.FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+  env.FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+  env.FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+  env.FIREBASE_MESSAGING_SENDER_ID =
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+  env.FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+  env.FIREBASE_MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+}
+
+console.log('env.FIREBASE_MEASUREMENT_ID', env.FIREBASE_MEASUREMENT_ID);
 
 const firebaseConfig = {
   apiKey: env.FIREBASE_API_KEY,
@@ -47,17 +50,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
 auth.useDeviceLanguage();
 const functions = getFunctions(app);
 const db = getFirestore(app);
 
-export function firebase() {
-  return {
-    app,
-    analytics,
-    auth,
-    functions,
-    db,
-  };
-}
+export { app, analytics, auth, functions, db };
