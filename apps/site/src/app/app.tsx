bda754020f2b6334bff789a3715ad83946e537f7
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-20 19:09:39 -0300

    import fonts on site

--- apps/site/src/app/app.tsx
+++ apps/site/src/app/app.tsx
@@ -4,6 +4,9 @@ import { Hero } from '@postgpt/commonui';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
+import '@fontsource/lora/400.css';
+import '@fontsource/work-sans/400.css';
+
 export function App() {
   return (
     <>
