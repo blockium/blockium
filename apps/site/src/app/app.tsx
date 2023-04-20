3fba37d41dadf10af3d0e6549867186fb3bc2e1a
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-20 19:38:31 -0300

    Initial theme config

--- apps/site/src/app/app.tsx
+++ apps/site/src/app/app.tsx
@@ -1,18 +1,24 @@
-import { Hero } from '@postgpt/commonui';
+// import { Hero } from '@postgpt/commonui';
 // import { app } from '@postgpt/firebase';
+
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import '@fontsource/lora/400.css';
-import '@fontsource/work-sans/400.css';
-
 export function App() {
   return (
     <>
-      <h1>Post GPT</h1>
-      <Hero />
-      <div />
+      <Typography variant="h1" color="initial">
+        Transforme sua criatividade em conteúdo incrível para o Instagram com o
+        Post GPT
+      </Typography>
+      <Box>Para advogados...</Box>
+      <Button variant="text" color="primary">
+        Testar Grátis
+      </Button>
     </>
   );
 }
--- libs/theme/src/lib/theme.tsx
+++ libs/theme/src/lib/theme.tsx
@@ -1,14 +1,86 @@
-import styles from './theme.module.scss';
+import React, { PropsWithChildren } from 'react';
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from 'react-router-dom';
 
-/* eslint-disable-next-line */
-export interface ThemeProps {}
+import {
+  createTheme,
+  ThemeProvider as BaseThemeProvider,
+  CssBaseline,
+  PaletteOptions,
+} from '@mui/material';
+import type { LinkProps } from '@mui/material/Link';
 
-export function Theme(props: ThemeProps) {
+import '@fontsource/lora/400.css';
+import '@fontsource/work-sans/400.css';
+
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return <RouterLink ref={ref} to={href} {...other} />;
+});
+
+type ThemeConfig = {
+  app: PaletteOptions;
+  site: PaletteOptions;
+  [key: string]: PaletteOptions;
+};
+
+const themes: ThemeConfig = {
+  app: {
+    primary: {
+      main: '#53CCA5',
+    },
+    secondary: {
+      main: '#0F172A',
+    },
+  },
+  site: {
+    primary: {
+      main: '#53CCA5',
+    },
+    secondary: {
+      main: '#0F172A',
+    },
+  },
+};
+
+const { host } = window.location;
+const config = host.split('.')[0];
+const palette = themes[config] || themes.site;
+
+const theme = createTheme({
+  palette,
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        sizeSmall: { padding: 6 },
+      },
+    },
+  },
+});
+
+export function ThemeProvider({ children }: PropsWithChildren) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Theme!</h1>
-    </div>
+    <BaseThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </BaseThemeProvider>
   );
 }
 
-export default Theme;
+export default ThemeProvider;
