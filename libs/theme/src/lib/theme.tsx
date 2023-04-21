9808ba4d15962ae5697cdd79145e285adcf9b044
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-21 11:45:06 -0300

    Configure typography fonts

--- libs/theme/src/lib/theme.tsx
+++ libs/theme/src/lib/theme.tsx
@@ -10,6 +10,7 @@ import {
   CssBaseline,
   PaletteOptions,
 } from '@mui/material';
+import { TypographyOptions } from '@mui/material/styles/createTypography';
 import type { LinkProps } from '@mui/material/Link';
 
 import '@fontsource/lora/400.css';
@@ -24,13 +25,13 @@ const LinkBehavior = React.forwardRef<
   return <RouterLink ref={ref} to={href} {...other} />;
 });
 
-type ThemeConfig = {
+type PaletteConfig = {
   app: PaletteOptions;
   site: PaletteOptions;
   [key: string]: PaletteOptions;
 };
 
-const themes: ThemeConfig = {
+const palettes: PaletteConfig = {
   app: {
     primary: {
       main: '#53CCA5',
@@ -51,10 +52,22 @@ const themes: ThemeConfig = {
 
 const { host } = window.location;
 const config = host.split('.')[0];
-const palette = themes[config] || themes.site;
+const palette = palettes[config] || palettes.site;
+
+const typography: TypographyOptions = {
+  fontFamily: [
+    '"Work Sans"',
+    '"Lora',
+    'Roboto',
+    '"Helvetica Neue"',
+    '-apple-system',
+    'sans-serif',
+  ].join(','),
+};
 
 const theme = createTheme({
   palette,
+  typography,
   components: {
     MuiLink: {
       defaultProps: {
