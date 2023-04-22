67ec74713cf64911853c1523647a38e64801fa5d
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-22 09:16:07 -0300

    Add proper fonts in theme

--- libs/theme/src/lib/theme.tsx
+++ libs/theme/src/lib/theme.tsx
@@ -9,12 +9,10 @@ import {
   ThemeProvider as BaseThemeProvider,
   CssBaseline,
   PaletteOptions,
+  responsiveFontSizes,
 } from '@mui/material';
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 import type { LinkProps } from '@mui/material/Link';
-
-import '@fontsource/lora/400.css';
-import '@fontsource/work-sans/400.css';
 
 const LinkBehavior = React.forwardRef<
   HTMLAnchorElement,
@@ -54,38 +52,70 @@ const { host } = window.location;
 const config = host.split('.')[0];
 const palette = palettes[config] || palettes.site;
 
+const fontFamilyLora = {
+  fontFamily: 'Lora',
+};
 const typography: TypographyOptions = {
   fontFamily: [
-    '"Work Sans"',
-    '"Lora',
+    'Work Sans',
     'Roboto',
     '"Helvetica Neue"',
+    'Lora',
     '-apple-system',
     'sans-serif',
   ].join(','),
+  h1: fontFamilyLora,
+  h2: fontFamilyLora,
+  h3: fontFamilyLora,
+  h4: fontFamilyLora,
+  h5: fontFamilyLora,
+  h6: fontFamilyLora,
+  htmlFontSize: 10,
 };
 
-const theme = createTheme({
-  palette,
-  typography,
-  components: {
-    MuiLink: {
-      defaultProps: {
-        component: LinkBehavior,
-      } as LinkProps,
-    },
-    MuiButtonBase: {
-      defaultProps: {
-        LinkComponent: LinkBehavior,
+const theme = responsiveFontSizes(
+  createTheme({
+    palette,
+    typography,
+    components: {
+      MuiLink: {
+        defaultProps: {
+          component: LinkBehavior,
+        } as LinkProps,
+      },
+      MuiButtonBase: {
+        defaultProps: {
+          LinkComponent: LinkBehavior,
+        },
+      },
+      MuiTableCell: {
+        styleOverrides: {
+          sizeSmall: { padding: 6 },
+        },
       },
     },
-    MuiTableCell: {
-      styleOverrides: {
-        sizeSmall: { padding: 6 },
-      },
-    },
+  })
+);
+
+theme.typography.h1 = {
+  ...theme.typography.h1,
+  // fontSize: '8.4rem',
+  [theme.breakpoints.up('xl')]: {
+    fontSize: '8.4rem',
   },
-});
+  [theme.breakpoints.down('xl')]: {
+    fontSize: '7.2rem',
+  },
+  [theme.breakpoints.down('lg')]: {
+    fontSize: '6.4rem',
+  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '5.6rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '4.8rem',
+  },
+};
 
 export function ThemeProvider({ children }: PropsWithChildren) {
   return (
