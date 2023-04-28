12d2a28e3e4154faa74f0a03c5f6dc3d572d3dd1
Marcos Pereira <mluiz.pereira@gmail.com>
2023-04-28 12:53:27 -0300

    Remove React StrictMode

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider } from '@postgpt/i18n';
 import { ThemeProvider } from '@postgpt/theme';
 
-import App from './app/app';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <IntlProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </IntlProvider>
-  </StrictMode>
+  // <StrictMode>
+  <IntlProvider>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </IntlProvider>
+  // </StrictMode>
 );
