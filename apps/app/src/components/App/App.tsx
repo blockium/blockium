120c0055deb1facbd3df88c15beb499d0a6886bd
Marcos Pereira <mluiz.pereira@gmail.com>
2023-05-12 12:20:51 -0300

    Add logout to home screen

--- apps/app/src/components/App/App.tsx
+++ apps/app/src/components/App/App.tsx
@@ -1,12 +1,28 @@
 import { Outlet } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+import { auth } from '@postgpt/firebase';
+import { signOut } from 'firebase/auth';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './App.module.scss';
 
 export function App() {
+  const logout = async () => {
+    sessionStorage.clear();
+    if (auth.currentUser) {
+      await signOut(auth);
+    }
+    window.location.reload();
+  };
+
   return (
     <div>
       <h1>Welcome to app!</h1>
+
+      <Button onClick={logout} variant="contained" color="primary">
+        Sair
+      </Button>
 
       <Outlet />
     </div>
