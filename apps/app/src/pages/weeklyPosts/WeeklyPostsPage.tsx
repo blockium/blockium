6ffd3117f1276a3f201d316051f56286c3ef5edf
Marcos Pereira <mluiz.pereira@gmail.com>
2023-08-05 15:49:21 -0300

    does not automatically generate posts on weekend

--- apps/app/src/pages/weeklyPosts/WeeklyPostsPage.tsx
+++ apps/app/src/pages/weeklyPosts/WeeklyPostsPage.tsx
@@ -8,6 +8,7 @@ import { PostFormat, PostType } from '@postgpt/types';
 export const WeeklyPostsPage: React.FC = (props) => {
   const { isoStartDate, topic, format, type, character } = useParams();
   const [daysOfWeek, setDaysOfWeek] = useState<Date[]>([]);
+  const [daysOfWeekend, setDaysOfWeekend] = useState<Date[]>([]);
 
   useEffect(() => {
     const startDate = isoStartDate
@@ -15,22 +16,34 @@ export const WeeklyPostsPage: React.FC = (props) => {
       : startOfWeek(new Date());
 
     const daysOfWeek = [];
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 5; i++) {
       daysOfWeek.push(addDays(startDate, i));
     }
     setDaysOfWeek(daysOfWeek);
+
+    const daysOfWeekend = [];
+    for (let i = 5; i < 7; i++) {
+      daysOfWeekend.push(addDays(startDate, i));
+    }
+    setDaysOfWeekend(daysOfWeekend);
+    //
   }, [isoStartDate]);
 
   return (
-    <Stack gap="64px">
+    <Stack gap="96px">
       {daysOfWeek.map((date) => (
         <DayPostsView
+          key={date.toISOString()}
           date={date}
           topic={topic}
           format={format !== 'undefined' ? (format as PostFormat) : undefined}
           type={type !== 'undefined' ? (type as PostType) : undefined}
           character={character}
         />
+      ))}
+      {/* Does not generate automatically for Saturday and Sunday */}
+      {daysOfWeekend.map((date) => (
+        <DayPostsView key={date.toISOString()} date={date} />
       ))}
     </Stack>
   );
