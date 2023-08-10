2fed7fef91dc9db26480d4dcf3ffd1d2b284fc9e
Marcos Pereira <mluiz.pereira@gmail.com>
2023-08-10 18:36:00 -0300

    Move useExtendNavbar to hooks

--- libs/ui-calendar/src/lib/Calendar/CalendarView.tsx
+++ libs/ui-calendar/src/lib/Calendar/CalendarView.tsx
@@ -8,103 +8,15 @@ import {
 } from 'react';
 import { useIntersection } from 'react-use';
 import { addMonths, startOfMonth } from 'date-fns';
-import { Box, Button, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
-import { MonthYearPicker, useCurrentDate } from '@postgpt/ui-common';
-import {
-  DRAWER_WIDTH,
-  useNavbarExtraLine,
-  useToolbarExtra,
-} from '@postgpt/ui-mininal-tmpl';
-import { msg } from '@postgpt/i18n';
+import { useCurrentDate } from '@postgpt/ui-common';
 
 import CalendarMonth from './CalendarMonth'; // Import the MonthView component
 import { useCalendarCache, CalendarCache } from '../hooks/useCalendarCache';
+import { useExtendNavbar } from '../hooks';
 
 const MONTHS_TO_ADD = 12;
-
-// TODO: Move to useExtendNavbar hook
-const useExtendNavbar = () => {
-  const [, setCurrentDate] = useCurrentDate();
-  const [, setToolbarExtra] = useToolbarExtra();
-  const [, setNavbarExtraLine] = useNavbarExtraLine();
-
-  useEffect(() => {
-    const toolbarExtra = (
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          gap: '2rem',
-          marginRight: (theme) => theme.spacing(2),
-        }}
-      >
-        <MonthYearPicker label={msg('app.page.calendar.filter.date')} />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setCurrentDate(new Date())}
-        >
-          {msg('app.button.today')}
-        </Button>
-      </Box>
-    );
-    setToolbarExtra(toolbarExtra);
-
-    const navbarExtraLine = (
-      <Box
-        sx={{
-          paddingTop: (theme) => theme.spacing(2),
-          paddingLeft: (theme) => theme.spacing(2),
-          paddingRight: (theme) => theme.spacing(2),
-
-          width: (theme) => {
-            const { lg } = theme.breakpoints.values;
-            return {
-              xs: '100%',
-              lg: `calc(${lg}-${DRAWER_WIDTH})`,
-              xl: `calc(${lg}px - ${theme.spacing(6)})`,
-            };
-          },
-
-          margin: '0 auto',
-          display: 'grid',
-          justifyItems: 'center',
-          gridTemplateColumns: 'repeat(7, 1fr)',
-          gap: 2,
-        }}
-      >
-        {[
-          msg('app.monday-short'),
-          msg('app.tuesday-short'),
-          msg('app.wednesday-short'),
-          msg('app.thursday-short'),
-          msg('app.friday-short'),
-          msg('app.saturday-short'),
-          msg('app.sunday-short'),
-        ].map((day, index) => (
-          <Typography
-            color={(theme) =>
-              theme.palette.mode === 'light' ? 'primary.dark' : 'primary.light'
-            }
-            key={index}
-            component="div"
-            variant="subtitle1"
-          >
-            {day}
-          </Typography>
-        ))}
-      </Box>
-    );
-    setNavbarExtraLine(navbarExtraLine);
-
-    return () => {
-      setToolbarExtra(<div></div>);
-      setNavbarExtraLine(<div></div>);
-    };
-    //
-  }, [setCurrentDate, setNavbarExtraLine, setToolbarExtra]);
-};
 
 interface ICalendarViewProps {
   onWeekClick?: (weekStartDate: Date, element: HTMLElement | null) => void;
@@ -114,6 +26,7 @@ interface ICalendarViewProps {
 
 // No priority:
 // TODO: Add onDayClick callback to Calendar, so it can be used in other projects.
+// TODO: Allow infinite scrolling on top
 export const CalendarView: React.FC<ICalendarViewProps> = ({
   onWeekClick,
   fetchMonthData,
---
+++ libs/ui-calendar/src/lib/hooks/useExtendNavbar.tsx
@@ -0,0 +1,94 @@
+import { useEffect } from 'react';
+import { Box, Button, Typography } from '@mui/material';
+
+import { MonthYearPicker, useCurrentDate } from '@postgpt/ui-common';
+import {
+  DRAWER_WIDTH,
+  useNavbarExtraLine,
+  useToolbarExtra,
+} from '@postgpt/ui-mininal-tmpl';
+import { msg } from '@postgpt/i18n';
+
+export const useExtendNavbar = () => {
+  const [, setCurrentDate] = useCurrentDate();
+  const [, setToolbarExtra] = useToolbarExtra();
+  const [, setNavbarExtraLine] = useNavbarExtraLine();
+
+  useEffect(() => {
+    const toolbarExtra = (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '2rem',
+          marginRight: (theme) => theme.spacing(2),
+        }}
+      >
+        <MonthYearPicker label={msg('app.page.calendar.filter.date')} />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setCurrentDate(new Date())}
+        >
+          {msg('app.button.today')}
+        </Button>
+      </Box>
+    );
+    setToolbarExtra(toolbarExtra);
+
+    const navbarExtraLine = (
+      <Box
+        sx={{
+          paddingTop: (theme) => theme.spacing(2),
+          paddingLeft: (theme) => theme.spacing(2),
+          paddingRight: (theme) => theme.spacing(2),
+
+          width: (theme) => {
+            const { lg } = theme.breakpoints.values;
+            return {
+              xs: '100%',
+              lg: `calc(${lg}-${DRAWER_WIDTH})`,
+              xl: `calc(${lg}px - ${theme.spacing(6)})`,
+            };
+          },
+
+          margin: '0 auto',
+          display: 'grid',
+          justifyItems: 'center',
+          gridTemplateColumns: 'repeat(7, 1fr)',
+          gap: 2,
+        }}
+      >
+        {[
+          msg('app.monday-short'),
+          msg('app.tuesday-short'),
+          msg('app.wednesday-short'),
+          msg('app.thursday-short'),
+          msg('app.friday-short'),
+          msg('app.saturday-short'),
+          msg('app.sunday-short'),
+        ].map((day, index) => (
+          <Typography
+            color={(theme) =>
+              theme.palette.mode === 'light' ? 'primary.dark' : 'primary.light'
+            }
+            key={index}
+            component="div"
+            variant="subtitle1"
+          >
+            {day}
+          </Typography>
+        ))}
+      </Box>
+    );
+    setNavbarExtraLine(navbarExtraLine);
+
+    return () => {
+      setToolbarExtra(<div></div>);
+      setNavbarExtraLine(<div></div>);
+    };
+    //
+  }, [setCurrentDate, setNavbarExtraLine, setToolbarExtra]);
+};
+
+export default useExtendNavbar;
