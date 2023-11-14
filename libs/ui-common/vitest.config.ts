069e7190bd71522f1e92fc1517f965b6cdef0df7
Marcos Pereira <mluiz.pereira@gmail.com>
2023-11-14 19:13:47 -0300

    Add i18n to vitest.config files

--- libs/ui-common/vitest.config.ts
+++ libs/ui-common/vitest.config.ts
@@ -1,4 +1,18 @@
 import * as matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './src/lib/locales/en/translation.json';
+
+await i18next.use(initReactI18next).init({
+  // debug: true,
+  lng: 'en',
+  resources: {
+    en: {
+      'ui-common': en,
+    },
+  },
+});
+
 expect.extend(matchers);
