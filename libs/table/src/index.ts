d26b4c2c871191db6e2a415878681654740f2503
Marcos Pereira <mluiz.pereira@gmail.com>
2023-12-12 20:09:57 -0300

    Fix Typescript error on build

--- libs/table/src/index.ts
+++ libs/table/src/index.ts
@@ -2,6 +2,9 @@ import { addResourceBundles } from '@blockium/i18n';
 import en from './lib/locales/en/translation.json';
 import pt_BR from './lib/locales/pt-BR/translation.json';
 
+// Load custom @mui/material types
+import '@blockium/theme';
+
 addResourceBundles([
   { lng: 'en', ns: 'table', resources: en },
   { lng: 'pt-BR', ns: 'table', resources: pt_BR },
