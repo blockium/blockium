f5815e21ffab4c131f211b83016f7f9ad949ccbc
Marcos Pereira <mluiz.pereira@gmail.com>
2023-12-15 10:55:46 -0300

    Fix a bug related to build

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -7,24 +7,24 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 const manualChunks = (id: string) => {
   // console.log(id);
-  if (
-    id.includes('criaty/libs/model') ||
-    id.includes('criaty/libs/ui-custom')
-  ) {
-    return '@criaty';
-  }
-
-  if (id.includes('@blockium') || id.includes('criaty/libs')) {
-    return '@blockium';
-  }
 
   if (id.includes('node_modules')) {
     return id.toString().split('node_modules/')[1].split('/')[0].toString();
   }
 
+  if (id.includes('libs')) {
+    return 'app-lib';
+  }
+
   if (id.includes('src')) {
     return 'app';
   }
+
+  // VERY IMPORTANT: Theorically, there is nothing more to return.
+  // However, without this, it will unlikely return errors like:
+  // - "Uncaught TypeError: 'X' is not a function" or
+  // - "Uncaught ReferenceError: Cannot access 'W' before initialization"
+  return 'vendor';
 };
 
 export default defineConfig({
