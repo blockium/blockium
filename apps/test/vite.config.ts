ad0a252bb9895937e1198c9896dd9ae1eec558d9
Marcos Pereira <mluiz.pereira@gmail.com>
2023-12-15 18:34:23 -0300

    Fix bug in build for test app

--- apps/test/vite.config.ts
+++ apps/test/vite.config.ts
@@ -5,13 +5,24 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 const manualChunks = (id: string) => {
   // console.log(id);
+
   if (id.includes('node_modules')) {
     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  }
+
+  if (id.includes('libs')) {
+    return 'app-lib';
   }
 
   if (id.includes('src')) {
     return 'app';
   }
+
+  // VERY IMPORTANT: Theorically, there is nothing more to return.
+  // However, without this, it will unlikely return errors like:
+  // - "Uncaught TypeError: 'X' is not a function" or
+  // - "Uncaught ReferenceError: Cannot access 'W' before initialization"
+  return 'vendor';
 };
 
 export default defineConfig({
