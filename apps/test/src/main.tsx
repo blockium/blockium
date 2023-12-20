250ea8388e51f6691f804564bf9fa4295d8e5ff2
Marcos Pereira <mluiz.pereira@gmail.com>
2023-12-20 19:19:43 -0300

    Init i18next previously

--- apps/test/src/main.tsx
+++ apps/test/src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './components/App/App';
+
+import { i18nInit } from '@blockium/i18n';
+i18nInit();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <StrictMode>
-    <App />
+    <Suspense>
+      <App />
+    </Suspense>
   </StrictMode>,
 );
