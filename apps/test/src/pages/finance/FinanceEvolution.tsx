2bf5b096f07b00e9dbfb5755bff7b9e104f56598
Marcos Pereira <mluiz.pereira@gmail.com>
2023-12-30 13:37:48 -0300

    Adjust FinanceEvolution

--- apps/test/src/pages/finance/FinanceEvolution.tsx
+++ apps/test/src/pages/finance/FinanceEvolution.tsx
@@ -6,7 +6,7 @@ export const FinanceEvolution: React.FC = () => {
   const theme = useTheme();
 
   return (
-    <ChartWidget title="Title" subheader="(+43%) than last year">
+    <ChartWidget title="Evolução" subheader="(+3%) mês a mês">
       <EvolutionChart
         showCurrency
         // height={500}
@@ -30,14 +30,12 @@ export const FinanceEvolution: React.FC = () => {
           {
             name: 'Receita',
             type: 'area',
-            // fill: 'gradient',
             // data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
             data: [350.567, 500, 550, 590, 650, 800],
           },
           {
             name: 'Despesas',
             type: 'line', //'column',
-            // fill: 'solid',
             // data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
             data: [430, 450, 440, 560, 560, 540],
           },
