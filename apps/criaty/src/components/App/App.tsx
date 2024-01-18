ba81615a418d8677732c9a5c2b67930b22d2a7a1
Marcos Pereira <mluiz.pereira@gmail.com>
2024-01-18 14:18:55 -0300

    Adjust criaty test

--- apps/criaty/src/components/App/App.tsx
+++ apps/criaty/src/components/App/App.tsx
@@ -132,11 +132,6 @@ export const App: React.FC = (props) => {
       loginMethods={['google']}
       loginLeftImageSrc="/images/login_768_1064.webp"
       loginTopImageSrc="/images/login_1064_768.webp"
-      // newWhatsAppSessionApi={import.meta.env.VITE_NEW_SESSION_URL}
-      // loginWhatsAppPhone={import.meta.env.VITE_CRIATY_PHONE}
-      // afterWhatsAppLoginApi={import.meta.env.VITE_AFTER_LOGIN_WHATSAPP_URL}
-      afterEmailLoginApi={import.meta.env.VITE_AFTER_LOGIN_EMAIL_URL}
-      // afterPhoneLoginApi={import.meta.env.VITE_AFTER_LOGIN_PHONE_URL}
     />
   );
 };
--- apps/criaty/src/components/index.ts
+++ apps/criaty/src/components/index.ts
@@ -1,4 +1,2 @@
 export * from './App';
-export * from './App2';
-export * from './App2Router';
 export * from './post';
