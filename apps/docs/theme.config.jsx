a0bfc3bc97288781e29a5b5e478949d09346f636
Marcos Pereira <mluiz.pereira@gmail.com>
2024-01-23 16:30:46 -0300

    Configure nextra

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,9 @@
+const themeConfig = {
+  logo: <span>Blockium Documentation</span>,
+  project: {
+    link: 'https://github.com/blockium/blockium',
+  },
+  // ... other theme options
+};
+
+export default themeConfig;
