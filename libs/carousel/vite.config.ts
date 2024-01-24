cb0873cd4717b6562a39769773f5cca843d0acc8
Marcos Pereira <mluiz.pereira@gmail.com>
2024-01-24 10:19:37 -0300

    Adjust carousel and chat config files

--- libs/carousel/vite.config.ts
+++ libs/carousel/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+// @ts-expect-error: untyped.
+import postprocess from '@stadtlandnetz/rollup-plugin-postprocess';
+import getSrcInputs from '../vite.config.utils';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/carousel',
@@ -13,15 +16,25 @@ export default defineConfig({
     nxViteTsPaths(),
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
+
+    // Remove import for external dependencies at the index files only.
+    // In the postprocess pluging, the index.ts files start with "export...",
+    // followed by the imports of external libs which can be dropped.
+    postprocess((param: { code: string; sourceMap: string; format: string }) =>
+      param.code.startsWith('export') ? [[/import [^;]*/, '']] : [],
+    ),
   ],
 
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
+
+  esbuild: {
+    minifyIdentifiers: false,
+  },
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
@@ -33,11 +46,26 @@ export default defineConfig({
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
+      // This will split code into different files, for tree shaking.
+      input: getSrcInputs(__dirname),
+      output: {
+        // This will split code into different files, for tree shaking.
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        '@mui/material',
+        '@mui/material/styles',
+        'react-i18next',
+        '@blockium/i18n',
+      ],
     },
   },
 
@@ -48,5 +76,6 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: './vitest.config.ts',
   },
 });
