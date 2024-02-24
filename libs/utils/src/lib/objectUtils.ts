3c9d9e4fca45fd13f8ae3728467cc810a2b7b6c8
Marcos Pereira <mluiz.pereira@gmail.com>
2024-02-24 10:00:02 -0300

    Make removeNullishFields recursive

--- libs/utils/src/lib/objectUtils.ts
+++ libs/utils/src/lib/objectUtils.ts
@@ -1,7 +1,11 @@
-export function removeNullishFields<T extends object>(objParam: T) {
+export function removeNullishFields<T extends object>(objParam: T): T {
   const obj: T = { ...objParam };
-  Object.keys(obj).forEach(
-    (k) => (obj as never)[k] == null && delete (obj as never)[k],
-  );
+  Object.keys(obj).forEach((k) => {
+    if ((obj as never)[k] == null) {
+      delete (obj as never)[k];
+    } else if (typeof (obj as never)[k] === 'object') {
+      (obj as never)[k] = removeNullishFields((obj as never)[k]);
+    }
+  });
   return obj;
 }
