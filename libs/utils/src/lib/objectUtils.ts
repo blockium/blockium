8595aad35927fd490beed49758bca997cda32b10
Marcos Pereira <mluiz.pereira@gmail.com>
2024-02-25 20:20:51 -0300

    Update removeNullishFields

--- libs/utils/src/lib/objectUtils.ts
+++ libs/utils/src/lib/objectUtils.ts
@@ -3,8 +3,8 @@ export function removeNullishFields<T extends object>(objParam: T): T {
   Object.keys(obj).forEach((k) => {
     if ((obj as never)[k] == null) {
       delete (obj as never)[k];
-    } else if (typeof (obj as never)[k] === 'object') {
-      (obj as never)[k] = removeNullishFields((obj as never)[k]);
+      // } else if (typeof (obj as never)[k] === 'object') {
+      //   (obj as never)[k] = removeNullishFields((obj as never)[k]);
     }
   });
   return obj;
