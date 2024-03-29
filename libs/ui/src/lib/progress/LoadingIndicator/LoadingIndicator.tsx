19fbc1cbc70888813a78f0117309bf740c0f69f1
Marcos Pereira <mluiz.pereira@gmail.com>
2024-03-29 18:22:25 -0300

    Loading indicator left on 0 or DRAWER_WIDTH depending on screen size

--- libs/ui/src/lib/progress/LoadingIndicator/LoadingIndicator.tsx
+++ libs/ui/src/lib/progress/LoadingIndicator/LoadingIndicator.tsx
@@ -11,11 +11,11 @@ export const LoadingIndicator = ({ children }: LoadingIndicatorProps) => {
     <Box position="relative" display="inline-flex">
       <CircularProgress size={80} />
       <Box
+        position="absolute"
         top={0}
         left={0}
         bottom={0}
         right={0}
-        position="absolute"
         display="flex"
         alignItems="center"
         justifyContent="center"
--- libs/ui/src/lib/progress/LoadingPage/LoadingPage.tsx
+++ libs/ui/src/lib/progress/LoadingPage/LoadingPage.tsx
@@ -1,18 +1,28 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 
 import { LoadingIndicator } from '../LoadingIndicator';
+
+const DRAWER_WIDTH = 280;
 
 interface LoadingPageProps {
   logo?: React.ReactElement;
 }
 
 export const LoadingPage: React.FC<LoadingPageProps> = ({ logo }) => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+
+  // left on 0 or DRAWER_WIDTH depending on screen size
   return (
     <Box
+      position="absolute"
+      top={0}
+      left={isDesktop ? DRAWER_WIDTH : 0}
+      bottom={0}
+      right={0}
       display="flex"
       justifyContent="center"
       alignItems="center"
-      sx={{ width: '100% ', height: '100vh' }}
     >
       <LoadingIndicator>{logo}</LoadingIndicator>
     </Box>
