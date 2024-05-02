a80569deaa33882bf047d8375dc9bc70a8d28cdb
Marcos Pereira <mluiz.pereira@gmail.com>
2024-05-02 19:15:50 -0300

    useCurrentDate keeps state between refreshs

--- libs/calendar/src/lib/hooks/useCurrentDate.ts
+++ libs/calendar/src/lib/hooks/useCurrentDate.ts
@@ -1,3 +1,21 @@
 import { createGlobalState } from 'react-use';
 
-export const useCurrentDate = createGlobalState<Date>(new Date());
+const useCurrentDateInt = createGlobalState<Date>(() => {
+  // Get the current date from local storage
+  const currentDateStr = localStorage.getItem('currentDate');
+
+  // Instantiate based on storage date or the current time
+  return currentDateStr ? new Date(currentDateStr) : new Date();
+});
+
+export const useCurrentDate = () => {
+  const [currentDate, setCurrentDateInt] = useCurrentDateInt();
+
+  // Before setting the current date, saves it in the local storage.
+  const setCurrentDate = (date: Date) => {
+    localStorage.setItem('currentDate', date.toISOString());
+    setCurrentDateInt(date);
+  };
+
+  return [currentDate, setCurrentDate];
+};
