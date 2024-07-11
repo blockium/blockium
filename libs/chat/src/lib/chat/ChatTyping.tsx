33929c19d199492f63bce107b15c880a0cf0b258
Marcos Pereira <mluiz.pereira@gmail.com>
2024-07-11 09:50:48 -0300

    Scroll to end to show chat typing

--- libs/chat/src/lib/chat/ChatTyping.tsx
+++ libs/chat/src/lib/chat/ChatTyping.tsx
@@ -1,13 +1,24 @@
 import { Box, Grid, useTheme } from '@mui/material';
 
 import { createDotFlashingStyles } from './createDotFlashingStyles';
+import { useEffect, useRef } from 'react';
 
 export const ChatTyping = () => {
   const theme = useTheme();
   const styles = createDotFlashingStyles(theme);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+    }
+  }, []);
 
   return (
-    <Grid container spacing={2} justifyContent="flex-start">
+    <Grid ref={ref} container spacing={2} justifyContent="flex-start">
       <Grid item>
         <Box width={(theme) => theme.spacing(4)}></Box>
       </Grid>
